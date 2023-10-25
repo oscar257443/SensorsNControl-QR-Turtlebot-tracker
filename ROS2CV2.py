06206dad8b5e224a67d0036eff8e0de4d45a8ba5
oscar257443 <oscargullickson@gmail.com>
2023-10-25 12:18:52 +1100

    Update ROS2CV2.py
    Minor update to fix some out of date code

--- ROS2CV2.py
+++ ROS2CV2.py
@@ -2,7 +2,7 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import rospy
-
+Camerafeed = None
 # Initialize ROS node
 rospy.init_node('image_processing_node', anonymous=True)
 
@@ -12,17 +12,15 @@ bridge = CvBridge()
 # Define a callback function to process the ROS image message
 def image_callback(msg):
     try:
+        global Camerafeed  
         # Convert the ROS Image message to an OpenCV image
-        cv_image = bridge.imgmsg_to_cv2(msg, "bgr8")
+        Camerafeed = bridge.imgmsg_to_cv2(msg, "bgr8")
         
-        # Now you can work with the OpenCV image (cv_image) here
-        # For example, you can display it using cv2.imshow() or perform image processing.
-
     except Exception as e:
         print(e)
 
 # Subscribe to the ROS image topic
-rospy.Subscriber("/your/image/topic", Image, image_callback)
+rospy.Subscriber("/camera/raw", Image, image_callback)
 
 # Spin to keep the script running
 rospy.spin()
